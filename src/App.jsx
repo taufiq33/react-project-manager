6ce70ecdb9bbb7ba9a7d2166c0f59d54e3de3665
Taufiq33 <taufiqhidayat1908@gmail.com>
2024-06-19 20:27:48 +0700

    Connect the "router" with ProjectPage

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,20 @@ import { useState } from "react";
 import DefaultContent from "./DefaultContent";
 import AddProject from "./AddProject";
 import Sidebar from "./Sidebar";
+import Project from "./Project";
 
 let dummyState = [
   {
+    id: 1,
     name: 'project 1',
+    description: 'Etiam euismod pharetra lacus, in molestie lorem gravida euismod. Curabitur et dapibus metus, sit amet lacinia velit. Phasellus tincidunt erat sed felis dapibus scelerisque. ',
     dueDate: 'May 9, 2028',
     tasks: ['task 1', 'task 2'],
   },
   {
+    id: 2,
     name: 'project 2',
+    description: 'Plan B of project 1',
     dueDate: 'December 9, 2024',
     tasks: [],
   }
@@ -30,6 +35,8 @@ function App() {
     content = <AddProject 
       onBackHome={handleBackToHome}
     />
+  } else if (view.page === 'project') {
+    content = <Project project={view.payload}/>
   }
 
   function handleAddProject() {
@@ -46,13 +53,20 @@ function App() {
     });
   }
 
+  function handleProjectDetail(project) {
+    setView({
+      page: 'project',
+      payload: project
+    });
+  }
+
   return (
     <>
-      <h1 className="my-8 text-center text-5xl font-bold">Project Management App</h1>
       <main className="h-screen flex">
         <Sidebar
           onAddProject={handleAddProject}
           projects={projects}
+          onViewDetailProject={handleProjectDetail}
         />
         {content}
       </main>
--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,15 +1,16 @@
 export default function Sidebar({
   onAddProject,
-  projects
+  projects,
+  onViewDetailProject,
 }) {
   return (
-    <section className="max-w-md bg-slate-900 text-white p-4 rounded-tr-lg">
+    <section className="max-w-md mt-10 bg-slate-900 text-white p-4 rounded-tr-lg">
       <h2 className="uppercase text-2xl my-8 mx-4 font-bold tracking-wide">Your Project</h2>
       <button onClick={onAddProject} className="py-2 px-4 bg-slate-600 rounded-lg block mx-auto my-10 hover:bg-slate-700">+ Add Project</button>
 
       <ul className="flex flex-col gap-3 justify-center items-center">
         {projects.map((project) => {
-          return <li key={project.name} className="p-1 bg-slate-800 hover:bg-slate-500 cursor-pointer rounded w-full text-center">{project.name}</li>
+          return <li onClick={() => onViewDetailProject(project)} key={project.name} className="p-1 bg-slate-800 hover:bg-slate-500 cursor-pointer rounded w-full text-center">{project.name}</li>
         })}
         
   
