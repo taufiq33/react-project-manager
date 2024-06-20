65792518292b93e0a5f005f1b5feeeadde738d0c
Taufiq33 <taufiqhidayat1908@gmail.com>
2024-06-20 10:35:33 +0700

    make sure all input form filled

--- src/AddProject.jsx
+++ src/AddProject.jsx
@@ -10,10 +10,11 @@ export default function AddProject({
   const dateRef = useRef('');
 
   function handleDueDateChange() {
-    setDueDate(new Date(dateRef.current.value).toLocaleDateString('en-US', {dateStyle: 'full'}));
+    setDueDate(new Date(dateRef.current.value).toLocaleDateString('en-US', { dateStyle: 'full' }));
   }
 
-  function onSave() {
+  function onSave(event) {
+    event.preventDefault();
     let newProject = {
       name: titleRef.current.value,
       description: descRef.current.value,
@@ -25,25 +26,27 @@ export default function AddProject({
 
   return (
     <section className="flex flex-col p-4 w-full">
-      <div className="flex justify-end p-2 gap-8 mb-2">
-        <button className="text-sm font-bold hover:text-slate-600 text-slate-900" onClick={onBackHome}>Cancel</button>
-        <button onClick={onSave} className="text-sm px-8 py-2 bg-slate-700 text-white rounded-lg shadow-md hover:bg-slate-500">Save</button>
-      </div>
-      <div>
-        <p>
-          <label htmlFor="title" className="text-slate-900 tracking-widest font-xl block mt-6 mb-1">TITLE</label>
-          <input ref={titleRef} className="border-0 border-b border-slate-400 rounded-md p-1 text-sm w-full bg-gray-200" name="title" type="text" />
-        </p>
-        <p>
-          <label htmlFor="description" className="text-slate-900 tracking-widest font-xl block mt-6 mb-1">DESCRIPTION</label>
-          <textarea ref={descRef} className="border-0 border-b border-slate-400 rounded-md p-1 text-sm w-full bg-gray-200" name="description" id=""></textarea>
-        </p>
-        <p>
-          <label htmlFor="dueDate" className="text-slate-900 tracking-widest font-xl block mt-6 mb-1">DUE DATE</label>
-          <input onChange={handleDueDateChange} ref={dateRef} className="border-0 border-b border-slate-400 rounded-md p-1 text-sm w-full bg-gray-200" type="date" name="dueDate" />
-          <span>{dueDate}</span>
-        </p>
-      </div>
+      <form onSubmit={onSave}>
+        <div className="flex justify-end p-2 gap-8 mb-2">
+          <button className="text-sm font-bold hover:text-slate-600 text-slate-900" onClick={onBackHome}>Cancel</button>
+          <button type="submit" className="text-sm px-8 py-2 bg-slate-700 text-white rounded-lg shadow-md hover:bg-slate-500">Save</button>
+        </div>
+        <div>
+          <p>
+            <label htmlFor="title" className="text-slate-900 tracking-widest font-xl block mt-6 mb-1">TITLE</label>
+            <input required ref={titleRef} className="border-0 border-b border-slate-400 rounded-md p-1 text-sm w-full bg-gray-200" name="title" type="text" />
+          </p>
+          <p>
+            <label htmlFor="description" className="text-slate-900 tracking-widest font-xl block mt-6 mb-1">DESCRIPTION</label>
+            <textarea required ref={descRef} className="border-0 border-b border-slate-400 rounded-md p-1 text-sm w-full bg-gray-200" name="description" id=""></textarea>
+          </p>
+          <p>
+            <label htmlFor="dueDate" className="text-slate-900 tracking-widest font-xl block mt-6 mb-1">DUE DATE</label>
+            <input required onChange={handleDueDateChange} ref={dateRef} className="border-0 border-b border-slate-400 rounded-md p-1 text-sm w-full bg-gray-200" type="date" name="dueDate" />
+            <span>{dueDate}</span>
+          </p>
+        </div>
+      </form>
     </section>
   )
 }
