efa0cfce36d7a7815970179e09f755081f518d95
Taufiq33 <taufiqhidayat1908@gmail.com>
2024-06-28 08:05:12 +0700

    feat: responsive sidebar and button toggle

--- src/DefaultContent.jsx
+++ src/DefaultContent.jsx
@@ -1,6 +1,6 @@
 import NoProjectImage from './assets/no-projects.png';
 
-export default function DefaultContent ({
+export default function DefaultContent({
   onAddProject
 }) {
   return (
@@ -11,6 +11,10 @@ export default function DefaultContent ({
       <p className='mb-8'>Select a project or get started with a new one</p>
 
       <button onClick={onAddProject} className="px-8 py-2 bg-slate-900 text-white shadow-md font-bold rounded hover:bg-slate-600">Create new Project</button>
+
+      <div className='text-slate-500 text-sm'>
+        <a target="_blank" href="https://icons8.com/icon/8113/menu">Menu</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+      </div>
     </section>
   )
 }
--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,20 +1,43 @@
+import { useRef, useState } from 'react';
+import BurgerMenuIcon from './assets/icons8-menu.svg';
+
 export default function Sidebar({
   onAddProject,
   projects,
   onViewDetailProject,
 }) {
-  return (
-    <section className="max-w-md mt-10 bg-slate-900 text-white p-4 rounded-tr-lg">
-      <h2 className="uppercase text-2xl my-8 mx-4 font-bold tracking-wide">Your Project</h2>
-      <button onClick={onAddProject} className="py-2 px-4 bg-slate-600 rounded-lg block mx-auto my-10 hover:bg-slate-700">+ Add Project</button>
+  const [toggleSidebar, setToggleSidebar] = useState(false);
+  const sidebarContent = useRef(null);
+  const parentSidebarContent = useRef(null);
+  const buttonRef = useRef(null);
 
-      <ul className="flex flex-col gap-3 justify-center items-center">
-        {projects.map((project) => {
-          return <li onClick={() => onViewDetailProject(project.id)} key={project.name} className="p-1 bg-slate-800 hover:bg-slate-500 cursor-pointer rounded w-full text-center">{project.name}</li>
-        })}
-        
-  
-      </ul>
+  function handleMenuClick() {
+    sidebarContent.current.classList.toggle('hidden');
+    parentSidebarContent.current.classList.toggle('w-10');
+    buttonRef.current.classList.toggle('w-8');
+    setToggleSidebar((prevState) => !prevState);
+  }
+
+  return (
+    <section ref={parentSidebarContent} className="w-10 sm:w-auto sm:mt-10 bg-slate-900 text-white sm:rounded-tr-lg">
+      <button ref={buttonRef} onClick={handleMenuClick} className='bg-slate-600 p-2 border-1 border-white mt-1 rounded-lg w-8 h-auto mx-auto block hover:bg-slate-400'>
+        {toggleSidebar && 'Close Sidebar' }
+        {!toggleSidebar && <img src={BurgerMenuIcon} alt="" />}
+      </button>
+
+      <div ref={sidebarContent} id="sidebar-content" className='p-4 hidden sm:block'>
+        <h2 className="uppercase text-2xl my-8 mx-4 font-bold tracking-wide">Your Project</h2>
+        <button onClick={onAddProject} className="py-2 px-4 bg-slate-600 rounded-lg block mx-auto my-10 hover:bg-slate-700">+ Add Project</button>
+
+        <ul className="flex flex-col gap-3 justify-center items-center">
+          {projects.map((project) => {
+            return <li onClick={() => onViewDetailProject(project.id)} key={project.name} className="p-1 bg-slate-800 hover:bg-slate-500 cursor-pointer rounded w-full text-center">{project.name}</li>
+          })}
+
+
+        </ul>
+      </div>
+
     </section>
   )
 }
